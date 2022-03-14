c061feeaaa76d4d0da06be7c15e9a80a5a2a809f
kkomall <101487601+kkomall@users.noreply.github.com>
2022-03-14 12:09:58 +1100

    final changes

--- first.java
+++ first.java
@@ -9,6 +9,10 @@ public class AddTwoNumbers {
 
         int num3=01;
         int ans=sum-num3;
-        System.out.println("final of these numbers: "+anss);
+        System.out.println("final of these numbers: "+ans);
+
+        int num4=9;
+        int idea=ans+3*num4;
+        System.out.println("New sum of these numbers: "+idea)
     }
 }
